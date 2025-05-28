1edb8854b0fceefef1b995804520f15e944733b2
Siowllience <1431947601@qq.com>
2025-05-29 00:43:44 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,4 @@
-"""
-Training script for IMDER
-dataset_name: Selecting dataset (mosi or mosei)
-seeds: This is a list containing running seeds you input
-mr: missing rate ranging from 0.1 to 0.7
-"""
+
 from run.run import ROHYDR_run
 
 ROHYDR_run(model_name='rohydr',
